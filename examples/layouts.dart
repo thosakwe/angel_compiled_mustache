ff1db4f2e0bda8d892ae45810a45d00ed4471e41
thislooksfun <thislooksfun@repbot.org>
2017-12-01 23:36:43 -0600

    Add examples

---
+++ examples/layouts.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+import 'package:angel_framework/angel_framework.dart';
+import 'package:angel_compiled_mustache/angel_compiled_mustache.dart';
+
+Future main() async {
+  Angel angel = new Angel();
+  await angel.configure(compiled_mustache(new Directory('views'), defaultLayout: 'home'));
+  
+  // This will render the file 'views/pages/hello.mustache'
+  // using the layout 'views/layouts/home.mustache'  (uses the default)
+  // with the context of 'name' = 'world'
+  var rendered = await angel.viewGenerator('hello', {'name': 'world'});
+  
+  // This will render the file 'views/pages/hello.mustache'
+  // using the layout 'views/layouts/main.mustache'  (uses the specified layout)
+  // with the context of 'name' = 'world'
+  var rendered = await angel.viewGenerator('hello', {'layout': 'main', 'name': 'world'});
+}
