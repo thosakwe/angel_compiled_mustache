9b0ff681fbad50f8968939b38d8e5b7f5a4e4f8d
thislooksfun <thislooksfun@repbot.org>
2017-12-10 20:14:50 -0600

    Switch from dart:io to package:file

--- lib/angel_compiled_mustache.dart
+++ lib/angel_compiled_mustache.dart
@@ -3,7 +3,9 @@
 library angel_compiled_mustache;
 
 import 'dart:async';
-import 'dart:io';
+import 'dart:io' as io;
+import 'package:file/file.dart' as pf;
+import 'package:file/local.dart' as pfl;
 import 'package:angel_framework/angel_framework.dart';
 import 'package:compiled_mustache/compiled_mustache.dart' show compile, CompiledTemplate;
 import 'package:path/path.dart' as path;
@@ -11,15 +13,15 @@ import 'package:path/path.dart' as path;
 part 'src/cache_controller.dart';
 
 /// Returns an [AngelConfigurer] that sets the app's [viewGenerator] to use mustache.
-compiled_mustache(Directory viewsDirectory,
+compiled_mustache(io.Directory viewsDirectory,
     {String fileExtension: '.mustache', String defaultLayout: 'main',
-     String layoutsPath: './layouts', String pagesPath: './pages', String partialsPath: './partials'}) {
+     String layoutsPath: './layouts', String pagesPath: './pages', String partialsPath: './partials', pf.FileSystem fileSystem: const pfl.LocalFileSystem()}) {
   
-  Directory layoutsDirectory  = new Directory(path.join(path.fromUri(viewsDirectory.uri), layoutsPath));
-  Directory pagesDirectory    = new Directory(path.join(path.fromUri(viewsDirectory.uri), pagesPath));
-  Directory partialsDirectory = new Directory(path.join(path.fromUri(viewsDirectory.uri), partialsPath));
+  pf.Directory layoutsDirectory  = fileSystem.directory(path.join(path.fromUri(viewsDirectory.uri), layoutsPath));
+  pf.Directory pagesDirectory    = fileSystem.directory(path.join(path.fromUri(viewsDirectory.uri), pagesPath));
+  pf.Directory partialsDirectory = fileSystem.directory(path.join(path.fromUri(viewsDirectory.uri), partialsPath));
   
-  _CacheController cache = new _CacheController(fileExtension, layoutsDirectory, pagesDirectory, partialsDirectory);
+  _CacheController cache = new _CacheController(fileExtension, layoutsDirectory, pagesDirectory, partialsDirectory, fileSystem);
 
   return (Angel app) async {
     app.viewGenerator = (String name, [Map data]) async {
